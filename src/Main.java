066f847d536684da7dd4aebc65145ca3824aabab
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-03-21 09:54:32 -0700

    created insertion sort and selection sort methods

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,29 @@ public class Main {
         System.out.println(numsList);
         Collections.sort(numsList);
         System.out.println(numsList);
-        System.out.println(Main.binarySearch(numsList, 200));
+        System.out.println(Main.binarySearch(numsList, 90));
+
+        int[] listToSort = {7,8,5,4,9,2};
+        for(int n: listToSort){
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        Main.selectionSort(listToSort);
+        for(int n: listToSort){
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        int[] listTwoSort = {5,8,1,3,9,6};
+        for(int n: listTwoSort){
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        Main.insertionSort(listTwoSort);
+        for(int n: listTwoSort){
+            System.out.print(n + " ");
+        }
+
     }
 
     public static int binarySearch(ArrayList<Integer> numsList, int target) {
@@ -41,6 +63,35 @@ public class Main {
             } else if (median < target) {
                 low = mid + 1;
                 mid = (high+low)/2;
+            }
+        }
+    }
+
+    public static void selectionSort(int[] list){
+        int len = list.length;
+        for(int i = 0; i < len - 1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < len; j++){
+                if(list[minIndex] > list[j]){
+                    minIndex = j;
+                }
+            }
+            int temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] arr){
+        int len = arr.length;
+        for(int i = 1; i < len; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                j--;
             }
         }
     }
